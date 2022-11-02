e09d8dedbad673dfaf4b0b4fe0cb3c443eec841f
Kewin Titus <kewintitus@gmail.com>
2022-11-02 18:28:49 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+//FILES
+// const textIn = fs.readFileSync('./input.txt', 'utf-8');
+// const textOut = `About avocado: ${textIn}. \nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+
+// console.log('File written');
+
+// //Non blocking
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   if (err) console.log('ERRORs');
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//       console.log(data3);
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+//         console.log('file written');
+//       });
+//     });
+//   });
+// });
+// console.log('reading file');
+
+/////////////////////////////////////
+// SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  console.log(req.url);
+
+  if (pathName === '/overview' || pathName === '/') {
+    res.end('This is the overview');
+  } else if (pathName === '/product') {
+    res.end('This is a product');
+  } else if (pathName === '/api') {
+    res.writeHead(200, { 'Content-type': 'application/json' });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'Hello world',
+    });
+    res.end('<h1> Page not found! </h1>');
+  }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to request on port');
+});
